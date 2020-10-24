e3b7bbdaaa85fba055eac13fa8c134510f346aa0
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2020-10-24 12:25:20 -0700

    Updates

---
+++ precache-manifest.b0f2bbfff15a9e2ef3c6bc7e5c5ec801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146d5ff2ca652335b8a01b5fe6b8ecd7",
+    "url": "/Frontend-Pagination-React/index.html"
+  },
+  {
+    "revision": "3296dcccbf11520bc9da",
+    "url": "/Frontend-Pagination-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8cd70ef55efc942e2ad7",
+    "url": "/Frontend-Pagination-React/static/js/2.0309f29f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Frontend-Pagination-React/static/js/2.0309f29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3296dcccbf11520bc9da",
+    "url": "/Frontend-Pagination-React/static/js/main.dab05e06.chunk.js"
+  },
+  {
+    "revision": "0d09ed6babbba045c442",
+    "url": "/Frontend-Pagination-React/static/js/runtime-main.dad26f70.js"
+  }
+]);
